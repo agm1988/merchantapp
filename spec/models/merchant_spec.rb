32cb5cb0657bdfa6ccf4e87be68adbb4a70164f7
Alexandr Gajdosh <alexandr@agm.com>
2019-12-11 18:07:23 +0200

    model specs

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  specify 'has a valid factory' do
+    expect(build(:merchant)).to be_valid
+  end
+
+  it 'associations' do
+    expect(subject).to have_many(:transactions)
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:status) }
+  end
+
+  specify 'uniqueness' do
+    is_expected.to validate_uniqueness_of(:email).case_insensitive
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  specify do
+    expect(build(:transaction)).to be_valid
+  end
+
+  it 'associations' do
+    expect(subject).to belong_to(:merchant)
+    expect(subject).to belong_to(:reference).optional
+    expect(subject).to have_one(:dependent)
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:amount) }
+    it { is_expected.to validate_presence_of(:status) }
+  end
 end
